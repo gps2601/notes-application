73037df0603996fdbe3f4219307028eeb0d8f8d8
Geoff Smith <smith260194@gmail.com>
2019-05-16 18:11:04 +0100

    Adding a class notebook

--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -1,1 +1,9 @@
 require 'notebook'
+
+describe Notebook do
+  it 'can initialize a notebook object' do
+    my_notebook = Notebook.new
+
+    expect(my_notebook).to be_a_kind_of(Notebook)
+  end
+end
