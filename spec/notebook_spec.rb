6fb9121e07034b5ab1868a3b1379532320f71c8d
Geoff Smith <smith260194@gmail.com>
2019-05-16 18:24:11 +0100

    Adding method add_note

--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -1,9 +1,13 @@
 require 'notebook'
 
 describe Notebook do
-  it 'can initialize a notebook object' do
-    my_notebook = Notebook.new
+  let(:my_notebook) { Notebook.new }
 
+  it 'can initialize a notebook object' do
     expect(my_notebook).to be_a_kind_of(Notebook)
+  end
+
+  it 'can respond to add note' do
+    expect(my_notebook).to respond_to(:add_note)
   end
 end
